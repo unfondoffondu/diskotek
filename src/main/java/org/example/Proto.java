b1810e4e03d50bc174082cd986f55f67a3d38a92
jeff <jaschowe@gmail.com>
2023-02-11 16:23:18 -0800

    New Feature branch to exhibit the registration process of Admin Users and their respective businesses.

---
+++ src/main/java/org/example/Proto.java
@@ -0,0 +1,43 @@
+/**
+ * Using Jackson, this Class handles file IO in JSON format
+ * Authored by Jeff Schowe for DiskoTek
+ * No warranty
+ */
+package org.example;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Map;
+
+import static java.nio.file.StandardOpenOption.*;
+
+public class Proto {
+    /**
+     * Simple class to read and write JSON.
+     * Methods
+     * WriteJSON(ObjectMapper o, Map m)    ---   deletes and rewrites the file
+     * ReadMasterRegistryJSON()            ---   reads the file
+     * @param cMapper
+     * @param cMap
+     * @throws IOException
+     */
+    public static void WriteJSON(ObjectMapper cMapper, Map cMap) throws IOException {
+        new PrintWriter("masterRegistry.json").close();
+        String json = cMapper.writerWithDefaultPrettyPrinter().writeValueAsString(cMap);
+        Files.write(new File("masterRegistry.json").toPath(), Collections.singletonList(json), APPEND);
+        System.out.println("Debug WriteJSON " + cMap.toString());
+    }
+    public static String ReadMasterRegistryJSON() throws IOException {
+        Path fileName = Path.of("masterRegistry.json");
+        return Files.readString(fileName);
+    }
+    protected static void addUserToMap(Map uMap, RegSimUserData cRegSimUserData) {
+        uMap.put(cRegSimUserData.userName, cRegSimUserData);
+
+    }
+}
+
---
+++ src/main/java/org/example/RegistrationSimulator.java
@@ -0,0 +1,112 @@
+/**
+ * RegistrationSimulator is a brief demonstration on how the RegSimUserData objects are built from and stored using JSON
+ * Author: Jeff Schowe
+ */
+package org.example;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class RegistrationSimulator {
+    static Scanner scanner = new Scanner(System.in);
+    public static void greetUser(Map uMap, ObjectMapper cMapper) {
+        System.out.println("Would you like to login(1) or register(2)?");
+        String input = scanner.nextLine();
+        if (Objects.equals(input, "1")) {
+            loginPortal(uMap, cMapper);
+        }
+        if (Objects.equals(input, "2")) {
+            registerUser(uMap, cMapper);
+        }
+    }
+    private static void registerUser(Map uMap, ObjectMapper cMapper) {
+
+        System.out.println("Welcome to the DiskoTek Beta Registration");
+        System.out.println("Enter desired username");
+        String desiredUsername = scanner.nextLine();
+        try {
+            if (uMap.get(desiredUsername) == null) {
+                buildUser(desiredUsername, uMap, cMapper);
+            }
+            System.out.println("That username already exists, please try again");
+            registerUser(uMap, cMapper);
+        } catch (Exception e) {
+            System.out.println("That username already exists, please try again");
+            registerUser(uMap,cMapper);
+        }
+    }
+    //TODO error checking, hash passwords/research JWT
+    private static void buildUser(String uName, Map uMap, ObjectMapper cMapper) throws IOException {
+        /**
+         *Handles all the input from new user and calls the instantiateUser method
+         */
+        RegSimUserData cRegSimUserData = new RegSimUserData();
+        String finalPW = null;
+        System.out.println("Enter password for " + uName);
+        String pw1 = scanner.nextLine();
+        System.out.println("Re-enter your password");
+        String pw2 = scanner.nextLine();
+        if (Objects.equals(pw1, pw2)) {finalPW = pw2;} else {buildUser(uName, uMap, cMapper);}
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Enter your e-mail address: ");
+        String userEmail = scanner.nextLine();
+        System.out.println("Enter your Age");
+        int age = scanner.nextInt();
+        instantiateUser(uMap, cRegSimUserData, name, age, finalPW, uName, userEmail);
+        writeAndExit(uMap, cMapper);
+        }
+
+    private static void writeAndExit(Map uMap, ObjectMapper cMapper) throws IOException {
+        Proto.WriteJSON(cMapper, uMap);
+        System.exit(17);
+    }
+
+    private static void instantiateUser(Map uMap, RegSimUserData regSimUserDataToInstantiate, String n, int a, String p, String uname, String email) {
+        /**
+         * This actually builds the new user and calls Proto.addUserToMap()
+         */
+        regSimUserDataToInstantiate.userName = uname;
+        regSimUserDataToInstantiate.password = p;
+        regSimUserDataToInstantiate.isAdmin = false;
+        regSimUserDataToInstantiate.name = n;
+        regSimUserDataToInstantiate.age = a;
+        regSimUserDataToInstantiate.email = email;
+        Proto.addUserToMap(uMap, regSimUserDataToInstantiate);
+    }
+    //TODO build registry for da club I ran out of time figuring out JSON, but the rest should be less painful. I need to
+    // encapsulate the logic from here and from Main to reuse the IO and object notation.
+    private static void registerEstablishment() {
+        System.out.println("This is where we will charge the business owner money and require them to prove they are who they say they are somehow.");
+        System.out.println("For now though, this is unimplemented, but will work much the same way as the buildUser method in this class, unfortunately I ran out of time..");
+    }
+    //TODO Bug I can't seem to figure out, it runs through the cycle twice after registration.
+    private static void loginPortal(Map uMap, ObjectMapper cMapper) {
+        System.out.println("Enter your username");
+        String userNameInput = scanner.nextLine();
+
+        try {
+            RegSimUserData regSimUserData = (RegSimUserData) uMap.get(userNameInput);
+            System.out.print("enter your password: ");
+            String passwordInput = scanner.nextLine();
+                                                                //Program exits in this block after logging in. Further features will move this elsewhere.
+            if (Objects.equals(passwordInput, regSimUserData.password)) {
+                System.out.println("Welcome to DiskoTek, first round is on us! ***CLING***");
+                System.out.println("debug RegSim.loginPortal -- at this point, the userData from the map has been authenticated against the username and password" +
+                        " and their class fields are accessible to this client, proving that the registration process works. ");
+                String _string = scanner.nextLine();
+                Proto.WriteJSON(cMapper,uMap);
+            } else  {
+                System.out.println("Invalid password or username not found.");
+                //loginPortal(uMap, cMapper);
+            }
+        } catch (Exception e){
+
+            System.out.println("User name not found, please try again.");
+            loginPortal(uMap, cMapper);
+        }
+    }
+}
